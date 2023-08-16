79922c3c1cb5a5a7843d8d8ad0f794914fa6bd9f
bautista12345678 <bautivaldes2014@gmail.com>
2023-08-16 01:47:32 -0300

    f

--- main.py
+++
@@ -1,6 +0,0 @@
-from src.source import main
-if __name__ == '__main__':
-    main()
-
-#si ven esto no busquen nada static porque no lo hay, perdon, aunque la idea de implementar una variable botín static no faltó.
-
